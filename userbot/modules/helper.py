6f579eb543ddaff115efe68d9ca6bbec5786172c
Wiki W <93371415+Wiki28@users.noreply.github.com>
2022-02-02 19:11:36 +0800

    Update helper.py

--- userbot/modules/helper.py
+++ userbot/modules/helper.py
@@ -8,15 +8,15 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern="^.helpmy$")
+@register(outgoing=True, pattern="^.helpme$")
 async def usit(e):
     await e.edit(
         f"__Hai {DEFAULTUSER} Kalau lu ga Tau cara buat__ **Memerintah** gua **Ketik** `.helpme` Atau `.help` atau Minta **Bantuan** __Ke:__\n"
-        f"✰ **Channel support :** [Fanda Project](t.me/fandaproject)\n"
-        f"✰ **Group Support :** [Fanda Support](t.me/fandasupport)\n"
-        f"✰ **Owner Repo :** [Fatur](t.me/uurfavboys1)\n"
-        f"✰ **Repo :** [DIOR-UBOT](https://github.com/DIORrios285/DIOR-UBOT)\n\n\n"
-        f"__Powered by Fanda Project__"
+        f"😑 **Channel support :** [Wiki Channel](t.me/WikiTapiChannel)\n"
+        f"😑 **Group Support :** [Wiki Group](t.me/WikiTapiGroup)\n"
+        f"😑 **Owner Repo :** [Wiki W](t.me/Hanya_W)\n"
+        f"😑 **Repo :** [DIOR-UBOT](https://github.com/Wiki28/Wiki-Userbot)\n\n\n"
+        f"__Powered by Wiki W__"
     )
 
 
@@ -25,12 +25,12 @@ async def usit(e):
 async def var(m):
     await m.edit(
         f"**Daftar Vars** {DEFAULTUSER}:\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/DIORrios285/DIOR-UBOT/DIOR-UBOT/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Wiki28/Wiki-Userbot/Wiki-Userbot/varshelper.txt)")
 
 
 CMD_HELP.update({
     "helper":
-    "`.helpmy`\
+    "`.helpme`\
 \nUsage: Bantuan Untuk DIOR-UBOT.\
 \n`.rvars`\
 \nUsage: Untuk Melihat Beberapa Daftar Vars."
